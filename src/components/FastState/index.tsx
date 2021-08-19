3bc9db2fe01ea20cbde15515bb3a0122020a73fe
Lisber Victores <lvpompa@gmail.com>
2021-08-19 09:37:55 -0300

    correção desafio

--- src/components/FastState/index.tsx
+++ src/components/FastState/index.tsx
@@ -1,27 +1,28 @@
 import React, { useState } from 'react';
+import { useRef } from 'react';
 import { useEffect } from 'react';
 
 function FastState() {
   const [value, setValue] = useState(0);
-  const [valueWithRef,setValueWithRef ]= useState(0);
+  const valueWithRef = useRef(0)
 
   useEffect(() => {
     const intervalId = setInterval(() => {
         setTimeout(() => {
-            setValue(value + 1);
-            setValueWithRef(valueWithRef + 1);
-           
+            setValue((oldValue) => oldValue + 1);
+            valueWithRef.current= valueWithRef.current + 1;
+            console.log(valueWithRef.current)
         }, 1000);
     }, 100);
     return () => clearInterval(intervalId);
-  }, [value, setValue, valueWithRef ])
+  }, [value, setValue])
 
   return (
     <div>
         <p>
           Fast State: {value}
           <br />
-          Fast State Correct: {valueWithRef}
+          Fast State Correct: {valueWithRef.current}
         </p>
     </div>
   );
